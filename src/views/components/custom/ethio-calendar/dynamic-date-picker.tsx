2b586199372a0434afaba2db751b1f22dc5f0c45
Abenezer Seyoum <abenezerseyoum7@gmail.com>
2024-07-02 14:18:28 +0300

    lint fix

--- src/views/components/custom/ethio-calendar/dynamic-date-picker.tsx
+++ src/views/components/custom/ethio-calendar/dynamic-date-picker.tsx
@@ -1,14 +1,21 @@
 import { useTranslation } from 'react-i18next';
-import EthioCalendar from 'src/views/components/forms/ethioCalendar/EthioCalendar';
 import DatePicker from 'react-datepicker';
+import EthioCalendar from './ethio-calendar';
+import EthiopianDate from './ethiopian-date';
 
-const DynamicDatePicker = ({ value, onChange, ...rest }) => {
+interface DynamicDatePickerProps {
+  value: Date|EthiopianDate;
+  onChange: (date: Date|EthiopianDate) => void;
+  [key: string]: any; // To allow any additional props
+}
+
+const DynamicDatePicker: React.FC<DynamicDatePickerProps> = ({ value, onChange, ...rest }) => {
   const { i18n } = useTranslation();
 
   return i18n.language === 'am' ? (
-    <EthioCalendar value={value} onChange={onChange} {...rest} />
+    <EthioCalendar value={value as EthiopianDate} onChange={onChange} {...rest} />
   ) : (
-    <DatePicker selected={value} onChange={onChange} dateFormat="dd/MM/yyyy" {...rest} />
+    <DatePicker selected={value as Date} onChange={onChange as (date: Date) => void} dateFormat="dd/MM/yyyy" {...rest} />
   );
 };
 
