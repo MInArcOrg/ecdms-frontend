e83d93532e0caa68a69f946220d4592d9d407d8a
rouge9 <robelged1992@gmail.com>
2024-07-15 01:03:44 -0900

    feat: Implement stakeholders page and components
    The code changes add the stakeholders page and related components, including the stakeholders list, stakeholders card, and stakeholders service. This allows users to view and manage stakeholders in the application.

---
+++ src/views/pages/stakeholders/stakeholders-card.tsx
@@ -0,0 +1,79 @@
+/* eslint-disable prettier/prettier */
+
+import {
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+  Typography,
+} from "@mui/material";
+import { Fragment } from "react";
+import { Stakeholders } from "src/types/stakeholders";
+import FileDrawer from "src/views/components/custom/files-drawer";
+import ModelActionComponent from "src/views/components/custom/model-actions";
+import RowOptions from "src/views/shared/listing/row-options";
+
+const StakeholdersCard = ({
+  stakeholders,
+  onEdit,
+  onDelete,
+  refetch,
+}: {
+  stakeholders: Stakeholders;
+  onEdit: (category: Stakeholders) => void;
+  onDelete: (id: string) => void;
+  t: any;
+  refetch: () => void;
+}) => {
+  return (
+    <Card>
+      <CardContent>
+        <Grid container alignItems="center" justifyContent="space-between">
+          <Grid item>
+            <Box sx={{ display: "flex" }}>
+              <Box>
+                <Typography variant="h5" component="div">
+                  {stakeholders.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {stakeholders.description}
+                </Typography>
+              </Box>
+            </Box>
+          </Grid>
+          <Grid item>
+            <CardActions style={{ justifyContent: "flex-end" }}>
+              <Fragment>
+                <Box>
+                  <FileDrawer id={stakeholders.id} type={"RESOURCE"} /> &nbsp;
+                  <Box sx={{ display: "flex" }}>
+                    <ModelActionComponent
+                      model="Position"
+                      model_id={stakeholders.id}
+                      refetchModel={refetch}
+                      resubmit={function (): void {
+                        throw new Error("Function not implemented.");
+                      }}
+                      title={""}
+                      postAction={function (): void {
+                        throw new Error("Function not implemented.");
+                      }}
+                    />
+                    <RowOptions
+                      onEdit={onEdit}
+                      onDelete={() => onDelete(stakeholders.id)}
+                      item={stakeholders}
+                      options={[]}
+                    />
+                  </Box>
+                </Box>
+              </Fragment>
+            </CardActions>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
+export default StakeholdersCard;
