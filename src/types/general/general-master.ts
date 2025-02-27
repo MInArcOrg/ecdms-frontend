365ef05799dbd4a7be41bbb8d09de98aaa2550fe
Abenezer01 <abenezerseyoum7@gmail.com>
2025-02-27 08:24:30 +0300

    ECDMS-136 area topography done!

--- src/types/general/general-master.ts
+++ src/types/general/general-master.ts
@@ -61,3 +61,12 @@ export interface EndwallTypeInlet {
   created_at?: Date;
   updated_at?: Date;
 }
+
+export interface AreaTopography {
+  id: string;
+  title?: string;
+  description?: string;
+  project_type_id: string;
+  created_at?: Date;
+  updated_at?: Date;
+}
