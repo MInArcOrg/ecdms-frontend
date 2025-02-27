681360513414ff350bc8a59d86140c75e7e1416b
Abenezer Seyoum <abenezerseyoum7@gmail.com>
2025-02-27 13:52:04 +0300

    lint fix

--- src/pages/master-data/general/project/pedestrian-facilities.tsx
+++ src/pages/master-data/general/project/pedestrian-facilities.tsx
@@ -1,6 +1,6 @@
 import { Fragment } from 'react';
 import GeneralMasterLayout from '../GeneralMasterLayout';
-import PedestrianFacilityMasterList from 'src/views/pages/master/general/project/area-topography-master/area-topography-master-list';
+import PedestrianFacilityMasterList from 'src/views/pages/master/general/project/pedestrian-facilities-master/pedestrian-facility-master-list';
 
 function GeneralProjectMasterData() {
     return (
