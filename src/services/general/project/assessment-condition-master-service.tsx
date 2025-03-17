e026a61a5483f75d494a973fb074a415cba030b8
Abenezer01 <abenezerseyoum7@gmail.com>
2025-03-17 11:42:54 +0300

    lint fix!

---
+++ src/services/general/project/assessment-condition-master-service.tsx
@@ -0,0 +1,51 @@
+import { AxiosResponse } from 'axios';
+import { AssessmentCondition } from 'src/types/general/general-master';
+import { GetRequestParam, IApiPayload, IApiResponse } from 'src/types/requests';
+import axiosServices from 'src/utils/axios';
+import { buildGetRequest } from 'src/utils/requests/get-request';
+import { buildPostRequest } from 'src/utils/requests/post-request';
+import { buildPutRequest } from 'src/utils/requests/put-request';
+
+const assessmentConditionMasterService = {
+  getAll: (params: GetRequestParam): Promise<IApiResponse<AssessmentCondition[]>> =>
+    buildGetRequest(`/masterdata/assessment-conditions`, params)
+      .then((response: AxiosResponse<IApiResponse>) => response.data)
+      .catch((error: any) => {
+        throw error;
+      }),
+
+  getOne: (idx: string, params: GetRequestParam): Promise<IApiResponse> =>
+    buildGetRequest(`/masterdata/assessment-conditions/${idx}`, params)
+      .then((response: AxiosResponse<IApiResponse>) => response.data)
+      .catch((error: any) => {
+        throw error;
+      }),
+  searchResource: (params: GetRequestParam) =>
+    buildGetRequest(`/masterdata/assessment-conditions-search`, params)
+      .then((response: AxiosResponse<IApiResponse>) => response.data.payload as unknown as AssessmentCondition[])
+      .catch((error: any) => {
+        throw error;
+      }),
+  delete: (idx: string): Promise<IApiResponse> =>
+    axiosServices
+      .delete(`/masterdata/assessment-conditions/${idx}`)
+      .then((response: AxiosResponse<IApiResponse>) => response.data)
+      .catch((error: any) => {
+        throw error;
+      }),
+
+  create: (body: IApiPayload<AssessmentCondition>): Promise<IApiResponse> =>
+    buildPostRequest(`/masterdata/assessment-conditions`, body, false)
+      .then((response: AxiosResponse<IApiResponse>) => response.data)
+      .catch((error: any) => {
+        throw error;
+      }),
+  update: (id: string, body: IApiPayload<AssessmentCondition>): Promise<IApiResponse> =>
+    buildPutRequest(`/masterdata/assessment-conditions/${id}`, body)
+      .then((response: AxiosResponse<IApiResponse>) => response.data)
+      .catch((error: any) => {
+        throw error;
+      })
+};
+
+export default assessmentConditionMasterService;
