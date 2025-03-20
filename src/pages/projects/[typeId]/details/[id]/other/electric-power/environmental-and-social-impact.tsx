6126233deb2dc6e22d8dd951968cdfd5db8b701c
Anduamlak Alehegne <anduamlakalehegne@gmail.com>
2025-03-21 00:03:19 +0300

    activeSubType Update

--- src/pages/projects/[typeId]/details/[id]/other/electric-power/environmental-and-social-impact.tsx
+++ src/pages/projects/[typeId]/details/[id]/other/electric-power/environmental-and-social-impact.tsx
@@ -11,7 +11,7 @@ function Index() {
   const baseUrl = `/projects/${typeId}/details/${id}/other`
   const activeMenu = 8
   const activeType = 4
-  const activeSubType = 81
+  const activeSubType = 85
 
   return (
     <ProjectOtherLayout
