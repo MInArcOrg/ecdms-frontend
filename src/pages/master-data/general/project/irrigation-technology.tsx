ec0378f5c4697e22bb86dd3e4e8aefe9e976a50a
Anduamlakalehegne <anduamlakalehegne@gmail.com>
2025-04-16 17:16:47 -0700

    ECDMS-333: Implement MD Irrigation technology crud functionality

---
+++ src/pages/master-data/general/project/irrigation-technology.tsx
@@ -0,0 +1,18 @@
+import { Fragment } from 'react';
+import { projectMasterModels } from 'src/constants/master-data/project-general-master-constants';
+import ProjectGeneralMasterList from 'src/views/pages/master/general/project/project-general-master/project-general-master-list';
+import GeneralMasterLayout from '../general-master-layout';
+
+function IrrigationTechnologyMasterData() {
+  return (
+    <div>
+      <GeneralMasterLayout>
+        <Fragment>
+          <ProjectGeneralMasterList projectMasterModel={projectMasterModels.irrigationTechnology} />
+        </Fragment>
+      </GeneralMasterLayout>
+    </div>
+  );
+}
+
+export default IrrigationTechnologyMasterData;
