9100542b8b0b99f6e6e7a048a1b8606a9bc12516
Abenezer01 <abenezerseyoum7@gmail.com>
2025-05-20 13:07:43 +0300

    feat(master-data): add new project master data components and translations
    Add new React components for track maintenance frequencies, scheduled maintenance activities, rehabilitation renewal methods, track inspection frequencies, and track safety measures. Also, update the English locale file with corresponding translations and extend the project master data constants to include these new models.

---
+++ src/pages/master-data/general/project/track-maintenance-frequencies.tsx
@@ -0,0 +1,18 @@
+import { Fragment } from 'react';
+import { projectMasterModels } from 'src/constants/master-data/project-general-master-constants';
+import ProjectGeneralMasterList from 'src/views/pages/master/general/project/project-general-master/project-general-master-list';
+import GeneralMasterLayout from '../general-master-layout';
+
+function GeneralProjectMasterData() {
+    return (
+        <div>
+            <GeneralMasterLayout>
+                <Fragment>
+                    <ProjectGeneralMasterList projectMasterModel={projectMasterModels.trackMaintenanceFrequency} />
+                </Fragment>
+            </GeneralMasterLayout>
+        </div>
+    );
+}
+
+export default GeneralProjectMasterData;
