060a909d67e58f70911627708b434f8a55e1f718
Abenezer Seyoum <abenezerseyoum7@gmail.com>
2025-09-22 23:55:49 +0300

    Refactor code style in utils.ts for consistency and readability

--- src/@core/layouts/utils.ts
+++ src/@core/layouts/utils.ts
@@ -1,6 +1,6 @@
 // ** Types
-import { NavGroup, NavLink } from "src/@core/layouts/types";
-import { NextRouter } from "next/router";
+import { NavGroup, NavLink } from 'src/@core/layouts/types';
+import { NextRouter } from 'next/router';
 
 /**
  * Check for URL queries as well for matching
@@ -9,18 +9,11 @@ import { NextRouter } from "next/router";
  * @param item
  * @param activeItem
  */
-export const handleURLQueries = (
-  router: NextRouter,
-  path: string | undefined,
-): boolean => {
+export const handleURLQueries = (router: NextRouter, path: string | undefined): boolean => {
   if (Object.keys(router.query).length && path) {
     const arr = Object.keys(router.query);
 
-    return (
-      router.asPath.includes(path) &&
-      router.asPath.includes(router.query[arr[0]] as string) &&
-      path !== "/"
-    );
+    return router.asPath.includes(path) && router.asPath.includes(router.query[arr[0]] as string) && path !== '/';
   }
 
   return false;
@@ -49,13 +42,7 @@ export const hasActiveChild = (item: NavGroup, currentURL: string): boolean => {
     const childPath = (child as NavLink).path;
 
     // Check if the child has a link and is active
-    if (
-      child &&
-      childPath &&
-      currentURL &&
-      (childPath === currentURL ||
-        (currentURL.includes(childPath) && childPath !== "/"))
-    ) {
+    if (child && childPath && currentURL && (childPath === currentURL || (currentURL.includes(childPath) && childPath !== '/'))) {
       return true;
     }
   }
@@ -71,19 +58,14 @@ export const hasActiveChild = (item: NavGroup, currentURL: string): boolean => {
  * @param openGroup
  * @param currentActiveGroup
  */
-export const removeChildren = (
-  children: NavLink[],
-  openGroup: string[],
-  currentActiveGroup: string[],
-) => {
+export const removeChildren = (children: NavLink[], openGroup: string[], currentActiveGroup: string[]) => {
   children.forEach((child: NavLink) => {
     if (!currentActiveGroup.includes(child.title)) {
       const index = openGroup.indexOf(child.title);
       if (index > -1) openGroup.splice(index, 1);
 
       // @ts-ignore
-      if (child.children)
-        removeChildren(child.children, openGroup, currentActiveGroup);
+      if (child.children) removeChildren(child.children, openGroup, currentActiveGroup);
     }
   });
 };
